374da03f6596569f13935edea64efa2b35a0db63
Aryles <aryles_77@icloud.com>
2024-04-16 16:54:59 +0200

    maj projet

--- asset/js/index.js
+++ asset/js/index.js
@@ -16,60 +16,103 @@ window.addEventListener('scroll', function(){
 
 
 
+//ANIMTXT
+
+
+let texte = "Aryles Ben Chabane - Développeur Web";
+let mot = ["Curieux", "Créatif", "Passioné"]
+let j = 0;
+let newtext = mot[j]
+let i = 0;
+let containhtml = document.querySelector(".txtanim");
+
+function ecrireTexte() {
+    if (i < texte.length) {
+        containhtml.innerHTML += texte.charAt(i);
+        i++;
+        setTimeout(ecrireTexte, 40);
+    } else {
+        setTimeout(effacerTexte, 3000);
+    }
+}
+
+function effacerTexte() {
+  let texteCourant = containhtml.innerHTML;
+  let longueur = texteCourant.length;
+  if (longueur > 0) {
+    containhtml.innerHTML = texteCourant.substring(0, longueur - 1);
+    setTimeout(effacerTexte, 40);
+  } else {
+    j = (j + 1) % mot.length; // Incrémentez j et assurez-vous qu'il reste dans les limites de la liste
+    texte = mot[j]; 
+    i = 0; // Réinitialisez i pour commencer l'écriture du nouveau mot
+    setTimeout(ecrireTexte, 1000); 
+}
+}
+
+ecrireTexte(); 
+
+
+
+
+
+
+
 // SLIDE
 
-let prev = document.querySelector('#prev');
-let next = document.querySelector('#next');
-let mainimg = document.querySelector('.mainimg')
-let textappear = document.querySelector('.textappear')
-let lien = document.querySelector('.link')
-let index = 1
+// let prev = document.querySelector('#prev');
+// let next = document.querySelector('#next');
+// let mainimg = document.querySelector('.mainimg')
+// let textappear = document.querySelector('.textappear')
+// let lien = document.querySelector('.link')
+// let index = 1
 
 
 
-next.addEventListener('click', function(){
-    index += 1;
-    if(index > 4){
-        index = 1
-    }
-    mainimg.src = `asset/img/projet/projet${index}.png`;
-    if(index == 1){
-        textappear.textContent = 'Mon premier projet personnel HTML CSS réalisé avec OpenClassroom'
-        lien.href = 'https://openclassrooms-student-center.github.io/1603881-creez-votre-site-web-avec-html5-et-css3/'
-    } else if(index == 2){
-        textappear.textContent = 'Mon premier site web interactif réalisé avec HTML CSS ainsi que JavaScript'
-        lien.href = 'https://site-e-commerce-js.vercel.app/'
-    } else if(index == 3){
-        textappear.textContent = "Reproduction d'une page Nike pour renforcer mes compétences en Javascript"
-        lien.href = 'https://nike-shoes-e-commerce.vercel.app/'
-    }
-    else if(index == 4){
-        textappear.textContent = "Site vitrine complet d'un spa avec formulaire de contact"
-        lien.href = 'https://spa-vitrine.vercel.app/'
-    }
-})
+// next.addEventListener('click', function(){
+//     index += 1;
+//     if(index > 4){
+//         index = 1
+//     }
+//     mainimg.src = `asset/img/projet/projet${index}.png`;
+//     if(index == 1){
+//         textappear.textContent = 'Mon premier projet personnel HTML CSS réalisé avec OpenClassroom'
+//         lien.href = 'https://openclassrooms-student-center.github.io/1603881-creez-votre-site-web-avec-html5-et-css3/'
+//     } else if(index == 2){
+//         textappear.textContent = 'Mon premier site web interactif réalisé avec HTML CSS ainsi que JavaScript'
+//         lien.href = 'https://site-e-commerce-js.vercel.app/'
+//     } else if(index == 3){
+//         textappear.textContent = "Reproduction d'une page Nike pour renforcer mes compétences en Javascript"
+//         lien.href = 'https://nike-shoes-e-commerce.vercel.app/'
+//     }
+//     else if(index == 4){
+//         textappear.textContent = "Site vitrine complet d'un spa avec formulaire de contact"
+//         lien.href = 'https://spa-vitrine.vercel.app/'
+//     }
+// })
 
-prev.addEventListener('click', function(){
-    index -= 1;
-    if(index < 1){
-        index = 4
-    }
-    mainimg.src = `asset/img/projet/projet${index}.png`;
-    if(index == 1){
-        textappear.textContent = 'Mon premier projet personnel HTML CSS réalisé avec OpenClassroom'
-        lien.href = 'https://openclassrooms-student-center.github.io/1603881-creez-votre-site-web-avec-html5-et-css3/'
-    } else if(index == 2){
-        textappear.textContent = 'Mon premier site web interactif réalisé avec HTML CSS et JavaScript'
-        lien.href = 'https://site-e-commerce-js.vercel.app/'
-    } else if(index == 3){
-        textappear.textContent = "Reproduction d'une page Nike pour renforcer mes compétences en Javascript"
-        lien.href = 'https://nike-shoes-e-commerce.vercel.app/'
-    } else if(index == 4){
-        textappear.textContent = "Site vitrine complet d'un spa avec formulaire de contact"
-        lien.href = 'https://spa-vitrine.vercel.app/'
-    }
+// prev.addEventListener('click', function(){
+//     index -= 1;
+//     if(index < 1){
+//         index = 4
+//     }
+//     mainimg.src = `asset/img/projet/projet${index}.png`;
+//     if(index == 1){
+//         textappear.textContent = 'Mon premier projet personnel HTML CSS réalisé avec OpenClassroom'
+//         lien.href = 'https://openclassrooms-student-center.github.io/1603881-creez-votre-site-web-avec-html5-et-css3/'
+//     } else if(index == 2){
+//         textappear.textContent = 'Mon premier site web interactif réalisé avec HTML CSS et JavaScript'
+//         lien.href = 'https://site-e-commerce-js.vercel.app/'
+//     } else if(index == 3){
+//         textappear.textContent = "Reproduction d'une page Nike pour renforcer mes compétences en Javascript"
+//         lien.href = 'https://nike-shoes-e-commerce.vercel.app/'
+//     } else if(index == 4){
+//         textappear.textContent = "Site vitrine complet d'un spa avec formulaire de contact"
+//         lien.href = 'https://spa-vitrine.vercel.app/'
+//     }
     
-})
+// })
+
 
 
 
@@ -151,3 +194,23 @@ let newyear = date.getFullYear()
 year.textContent = newyear
 
 
+
+
+
+
+// SCROLL REVEAL EFFECT
+
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.title-section', {delay:100, origin:'top'});
+sr.reveal('.about img', {delay:650, origin:'left'});
+sr.reveal('.about .box', {delay:450, origin:'right'});
+sr.reveal('.competences .box-container .box .skills', {delay:200, origin:'top'});
+sr.reveal('.realisation .container .box .img-container', {delay:400, origin:'top'});
+sr.reveal('.contact .box-container .rs', {delay:200, origin:'left'});
+sr.reveal('.contact .box-container .form', {delay:200, origin:'right'});
